bfd38463296a3cff9e246292cb0ab1e7f373aaca
Vic <victorsalazar.01.vv@gmail.com>
2026-01-07 13:25:53 -0700

    Working on getting HTML

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+function main (){
+    const args = process.argv.slice(2);
+    if ((args.length) < 1) {
+        console.error("error: missing base URL");
+        process.exit(1)
+    }
+    else if ((args.length) > 1 ) {
+        console.error ("error: too many base URLs");
+        process.exit(1)
+    }
+    else{
+        console.log(`starting at ${args[0]}`)
+        process.exit(0)
+    }
+
+
+}
+
+main();
